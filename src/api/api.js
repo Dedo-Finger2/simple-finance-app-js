214063272ac74f37dee51b898724130ad45bc929
antonio mauricio <antonioimportant@gmail.com>
2024-12-01 11:47:35 -0300

    feat: adds router

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,12 @@
 import http from "node:http";
 import { home } from "./routes/home.route.js";
+import * as router from "./router.js";
 
 export function bootstrap() {
-  const server = http.createServer((_request, response) => {
-    home(_request, response);
+  const server = http.createServer((request, response) => {
+    router.get("/", home);
+
+    router.handle(request, response);
   });
 
   return server;
---
+++ src/api/router.js
@@ -0,0 +1,23 @@
+/**
+ * @typedef { Object } Route
+ * @property { string } uri
+ * @property { "GET" | "POST" | "PUT" | "PATCH" | "DELETE" } method
+ * @property { Function } handler
+ */
+
+/** @type { Array<Route> } */
+export const routes = [];
+
+export function get(uri, handler) {
+  routes.push({ uri, handler, method: "GET" });
+}
+
+export function handle(request, response) {
+  for (const route of routes) {
+    if (request.url === route.uri && request.method === route.method) return route.handler(request, response);
+  }
+
+  response.writeHeader(404, { "content-type": "application/json" });
+  response.end(JSON.stringify({ message: "Not Found." }));
+  return;
+}
